c0f2b6423e7d30bd44aa1d9a65f742d009e2e182
Robetson <80962857+betthink@users.noreply.github.com>
2024-01-31 13:12:53 +0700

    change base url

--- src/utils/axioswithmultipart.jsx
+++ src/utils/axioswithmultipart.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const axiosWithMultipart = axios.create({
-  baseURL: "http://localhost",
+  // baseURL: "http://localhost",
   headers: {
     "Content-Type": "multipart/form-data",
   },
 
-  //   baseURL: "https://appfordev.com",
+    baseURL: "https://appfordev.com",
 });
