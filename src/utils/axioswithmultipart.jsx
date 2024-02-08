4f9cce09c2566816c453dcdd951226dbe6088db3
Robetson <80962857+betthink@users.noreply.github.com>
2024-02-08 14:53:17 +0700

    Update axioswithmultipart.jsx

--- src/utils/axioswithmultipart.jsx
+++ src/utils/axioswithmultipart.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosWithMultipart = axios.create({
   // baseURL: "http://localhost",
-  baseURL: "https://appfordev.com",
+  baseURL: "https://appfordev.com/",
   headers: {
     "Content-Type": "multipart/form-data",
   },
